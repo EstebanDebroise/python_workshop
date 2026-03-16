5b4e976eb734a57df745275420358f1121e6fbce
“DEBROISE <raxiou80@gmail.com>
2026-03-16 11:37:20 +0100

    question4

--- session1_1/main.py
+++ session1_1/main.py
@@ -1,4 +1,6 @@
 import random
+import os
+import hashlib
 
 def question1():
     randomlist = [[random.randint(1, 100) for i in range(10)] for j in range(10)]
@@ -25,17 +27,46 @@ def question2():
 def find_duplicate(randomlist):
     seen = []
     duplicates = []
-    for liste in randomlist:
-        for num in liste:
+    for lst in randomlist:
+        for num in lst:
             if num in seen and num not in duplicates:
                 duplicates.append(num)
             else:
                 seen.append(num)
     return duplicates
 
+def question4():
+        directory = "session1_1/testq4"
+        duplicates, hashes = find_duplicate_files(directory)
+        print("Duplicate files:")
+        print(duplicates)
+        print("File hashes:")
+        print(hashes)
+
+def find_duplicate_files(directory):
+        hashes = {}
+        duplicates = []
+        for root,_, files in os.walk(directory):
+            for filename in files:
+                filepath = os.path.join(root, filename)
+                try:
+                    with open(filepath, 'rb') as f:
+                        # I read the file and I verify if the hash of the file is already in the dictionary,
+                        # if it is, I add the file to the list of duplicates, otherwise I add the hash to the dictionary
+                        filehash = hashlib.md5(f.read()).hexdigest()
+                    if filehash in hashes:
+                        duplicates.append(filepath)
+                    else:
+                        hashes[filehash] = filepath
+                except Exception as e:
+                    pass
+        return duplicates, hashes
+
 if __name__ == "__main__":
     question1()
     print('-'*10)
     question2()
+    print('-'*10)
+    question4()
 
     
